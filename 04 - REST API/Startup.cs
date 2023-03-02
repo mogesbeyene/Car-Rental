2437bd9eaba7c63d2b646a0f26aad807ffb5c0e7
mogesbeyene <57324863+mogesbeyene@users.noreply.github.com>
2023-03-02 12:43:50 +0200

    Add files via upload

---
+++ 04 - REST API/Startup.cs
@@ -0,0 +1,55 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRental
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(setup => setup.AddPolicy("Application", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+            services.Configure<ApiBehaviorOptions>(o => o.SuppressModelStateInvalidFilter = true);
+            services.AddDbContext<CarRentalCompanyContext>(o => o.UseSqlServer(Configuration.GetConnectionString("localDB")));
+            //services.AddControllers();
+            //services.AddCors(setup => setup.AddPolicy("EntireWorld", policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+            services.AddTransient<BranchesLogic>();
+            services.AddTransient<RentalsLogic>();
+            services.AddTransient<UsersLogic>();
+            services.AddTransient<VehiclesLogic>();
+            services.AddTransient<VehicleTypesLogic>();
+
+            JwtHelper jwtHelper = new JwtHelper(Configuration.GetValue<string>("JWT:Key"));
+            services.AddSingleton(jwtHelper);
+
+            // Enable JWT Authentication: 
+            services.AddAuthentication(options => jwtHelper.SetAuthenticationOptions(options)).AddJwtBearer(options => jwtHelper.SetBearerOptions(options));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            //app.UseCors("EntireWorld");
+            app.UseCors();
+
+            app.UseAuthentication(); 
+            app.UseAuthorization();  
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
